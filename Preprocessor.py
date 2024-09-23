374e407192d5c4b0dd47f6f84f8895acf7721dcc
WHITELOTUS <126754757+WHITELOTUS4@users.noreply.github.com>
2024-09-23 18:27:22 +0530

    Update Preprocessor.py

--- Preprocessor.py
+++ Preprocessor.py
@@ -7,24 +7,14 @@ import io
 def sum(a, b):
     return a+b
 
-def sub(a, b):
-    return a-b
-
 def convert_jpg(image_data):
     image_bytes = base64.b64decode(image_data.split(",")[1])
     image = Image.open(io.BytesIO(image_bytes))
-    
-    # Convert image to RGB (in case it has an alpha channel)
     image = image.convert("RGB")
-    
-    # Create an in-memory buffer to store the image
     buffer = io.BytesIO()
     image.save(buffer, format="JPEG")
-    
-    # Encode the image in base64 and return as string
     jpg_image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
     return f"data:image/jpg;base64,{jpg_image_base64}"
-
 
 # def convert_jpg(image_data):
 #     header, base64_cdata = image_data.split(';base64,')
@@ -37,12 +27,7 @@ def convert_jpg(image_data):
 #     elif image.shape[2] == 1:  # If image is grayscale
 #         image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 #     success, encoded_image = cv2.imencode('.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
-   
 #     if not success:
 #         raise ValueError("Could not convert image to JPEG format.")
-
 #     jpg_base64 = base64.b64encode(encoded_image).decode('utf-8')
-
 #     return f"data:image/jpg;base64,{jpg_base64}"
-
-
